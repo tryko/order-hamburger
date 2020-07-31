325320505bb7b5b33630dea9455f1785b090a8b5
itay <itay83ko@gmail.com>
2020-07-31 15:51:48 +0300

    created components but didn't connect them

---
+++ src/components/MakeHamburger/MakeHamburger.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MakeHamburger = () => {
+    return <div></div>;
+};
+
+export default MakeHamburger;
---
+++ src/components/Toppings/Toppings.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Toppings = () => {
+    return <div></div>;
+};
+
+export default Toppings;
